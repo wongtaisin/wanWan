da4485aa5b7b24d72a016d8a4e3bdd94cd87eea4
wingddd <wongtaisin1024@gmail.com>
2025-08-25 10:50:20 +0800

    新增util的文件

--- controllers/expensesController.ts
+++ controllers/expensesController.ts
@@ -2,19 +2,39 @@
  * @Author: wingddd wongtaisin1024@gmail.com
  * @Date: 2025-08-21 16:38:22
  * @LastEditors: wingddd wongtaisin1024@gmail.com
- * @LastEditTime: 2025-08-21 17:08:01
+ * @LastEditTime: 2025-08-25 10:42:36
  * @FilePath: \express\controllers\expensesController.ts
  * @Description:
  *
  * Copyright (c) 2025 by wongtaisin1024@gmail.com, All Rights Reserved.
  */
 import mysql from '../db/mysql'
+import _util from '../util/util'
 const expensesService = require('../service/expensesService')
 
 // 添加花销
 exports.add = async (req: any, res: any, next: any) => {
-  let { eat, drink, play, glad, tolls, oil, parking, supermarket, online_shopping, create_date } =
-    req.body
+  let {
+    eat,
+    drink,
+    play,
+    glad,
+    tolls,
+    oil,
+    parking,
+    traffic,
+    supermarket,
+    online_shopping,
+    phone_bill,
+    red_packet,
+    create_date
+  } = req.body
+
+  const checkDate: any = await mysql.query(expensesService.checkDate, [create_date] as never[])
+
+  if (checkDate.length > 0) {
+    return res.status(400).json({ message: '日期已存在' })
+  }
 
   const params: any = [
     eat,
@@ -24,22 +44,20 @@ exports.add = async (req: any, res: any, next: any) => {
     tolls,
     oil,
     parking,
+    traffic,
     supermarket,
     online_shopping,
-    create_date
+    phone_bill,
+    red_packet
   ]
-
-  const checkDate: any = await mysql.query(expensesService.checkDate, [create_date] as never[])
-
-  if (checkDate.length > 0) {
-    return res.status(400).json({ message: '日期已存在' })
-  }
 
   let result = await mysql.query(expensesService.addExpenses, params)
   if (result) {
     res.json({
       msg: '添加成功',
-      data: result,
+      data: {
+        ...result
+      },
       code: 200
     })
   }
@@ -49,8 +67,15 @@ exports.add = async (req: any, res: any, next: any) => {
 exports.list = async (req: any, res: any) => {
   try {
     // 执行查询获取花销列表
-    const data = await mysql.query(expensesService.expensesAll)
-    res.json({ data })
+    const data: any = await mysql.query(expensesService.expensesAll)
+    data.forEach((item: any) => {
+      item.create_date = _util.formatDate(item.create_date, 'yyyy-MM-dd')
+    })
+    res.json({
+      code: 200,
+      data: data,
+      msg: '获取花销列表成功'
+    })
   } catch (error) {
     // 捕获并处理查询过程中的错误
     console.error('获取花销列表失败:', error)
--- service/expensesService.ts
+++ service/expensesService.ts
@@ -1,13 +1,23 @@
+/*
+ * @Author: wingddd wongtaisin1024@gmail.com
+ * @Date: 2025-08-21 16:38:48
+ * @LastEditors: wingddd wongtaisin1024@gmail.com
+ * @LastEditTime: 2025-08-25 10:11:28
+ * @FilePath: \express\service\expensesService.ts
+ * @Description:
+ *
+ * Copyright (c) 2025 by wongtaisin1024@gmail.com, All Rights Reserved.
+ */
 exports.expensesAll = `SELECT * FROM expenses;`
 
 exports.expensesById = `SELECT * FROM expenses WHERE id = ?;`
 
-exports.addExpenses = `INSERT INTO expenses (eat, drink, play, glad, tolls,oil, parking, supermarket, online_shopping, create_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, now())`
+exports.addExpenses = `INSERT INTO expenses (eat, drink, play, glad, tolls, oil, parking, traffic, supermarket, online_shopping, phone_bill, red_packet, create_date, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, now(), ?)`
 
-exports.updateExpenses = `UPDATE expenses SET eat = ?, drink = ?, play = ?, glad = ?, tolls = ?, oil = ?, parking = ?, supermarket = ?, online_shopping = ? WHERE id = ?`
+exports.updateExpenses = `UPDATE expenses SET eat = ?, drink = ?, play = ?, glad = ?, tolls = ?, oil = ?, parking = ?, traffic = ?, supermarket = ?, online_shopping = ?, phone_bill = ?, red_packet = ? WHERE id = ?`
 
 exports.deleteExpenses = `DELETE FROM expenses WHERE id = ?;`
 
 exports.deleteExpensesAll = `DELETE FROM expenses;`
 
-exports.checkDate = `SELECT * FROM user WHERE create_date = ?`
+exports.checkDate = `SELECT * FROM expenses WHERE create_date = ?`
