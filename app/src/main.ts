6b7d771da716957f38af5120bb3af46b0161f6c8
wingddd <wongtaisin1024@gmail.com>
2025-09-12 08:25:17 +0800

    Pinia，axios，sass新增

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,21 @@
-import { createSSRApp } from "vue";
-import App from "./App.vue";
+/*
+ * @Author: wingddd wongtaisin1024@gmail.com
+ * @Date: 2025-09-11 16:36:48
+ * @LastEditors: wingddd wongtaisin1024@gmail.com
+ * @LastEditTime: 2025-09-11 17:13:10
+ * @FilePath: \app\src\main.ts
+ * @Description:
+ *
+ * Copyright (c) 2025 by wongtaisin1024@gmail.com, All Rights Reserved.
+ */
+import { createSSRApp } from 'vue'
+import App from './App.vue'
+// import router from './router'
+import pinia from './store'
+
 export function createApp() {
-  const app = createSSRApp(App);
-  return {
-    app,
-  };
+  const app = createSSRApp(App)
+  app.use(pinia)
+
+  return { app }
 }
