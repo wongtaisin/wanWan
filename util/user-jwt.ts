baf7ee41fadb17c8b99c55907f2f786ac46b975e
wingddd <wongtaisin1024@gmail.com>
2025-12-16 19:17:55 +0800

    注册。登录白名单

--- util/user-jwt.ts
+++ util/user-jwt.ts
@@ -4,6 +4,6 @@ import { expressjwt } from 'express-jwt'
 const jwtAuth = expressjwt({
   secret: process.env.JWT_SECRET || 'wongtaisin', // 密匙
   algorithms: ['HS256'] // 签名算法
-}).unless({ path: ['/api/login', '/api/register'] }) // unless 设置jwt认证白名单
+}).unless({ path: ['/api/login/signIn', '/api/login/signUp'] }) // unless 设置jwt认证白名单
 
 export default jwtAuth
