1b3f65d2e6f42d0f855fe56dbc995ea711e2e403
wingddd <wongtaisin1024@gmail.com>
2025-12-31 15:33:19 +0800

    添加 crypto-js，使用哈希值，盐值 作为加密密码使用

--- src/middleware/login.ts
+++ src/middleware/login.ts
@@ -2,14 +2,15 @@
  * @Author: wingddd wongtaisin1024@gmail.com
  * @Date: 2025-08-25 11:02:53
  * @LastEditors: wingddd wongtaisin1024@gmail.com
- * @LastEditTime: 2025-11-25 16:45:55
- * @FilePath: \wanWan\middleware\login.ts
+ * @LastEditTime: 2025-12-31 15:21:27
+ * @FilePath: \wanWan\src\middleware\login.ts
  * @Description:
  *
  * Copyright (c) 2025 by wongtaisin1024@gmail.com, All Rights Reserved.
  */
 import mysql from '../db/mysql'
 import loginService from '../service/loginService'
+import { hashPassword, verifyPassword } from '../util/cryptoJS'
 import _util from '../util/util'
 
 /**
@@ -33,27 +34,37 @@ export const loginParams = async (req: any, res: any, next: any) => {
   }
 
   try {
-    // 查询数据库，验证账号密码
-    const user = (await mysql.query(loginService.verifyUser, [
-      user_name,
-      password
-    ] as never[])) as any
+    const resultUser = (await mysql.query(loginService.checkUser, [user_name] as never[])) as any
 
-    if (user.length === 0) {
+    const { password: hash, salt } = resultUser[0]
+
+    const isPasswordValid = verifyPassword(password, hash, salt)
+
+    if (!isPasswordValid) {
       return res.status(401).json({
         code: 401,
         message: '账号或密码不正确'
       })
     }
 
+    // 查询数据库，验证账号密码
+    // const result = (await mysql.query(loginService.verifyUser, [user_name, hash] as never[])) as any
+
+    // if (result.length === 0) {
+    //   return res.status(401).json({
+    //     code: 401,
+    //     message: '账号或密码不正确'
+    //   })
+    // }
+
     // 根据用户ID更新登录时间，并且获取 login_ip
     await mysql.query(loginService.updateLoginTimeAndGetIp, [
       _util.getClientIp(req),
-      user[0].user_id
+      resultUser[0].user_id
     ] as never[])
 
     // 验证通过，将用户信息添加到请求对象中
-    req.user = user[0]
+    req.user = resultUser[0]
     next()
   } catch (error) {
     console.error('验证失败:', error)
@@ -66,7 +77,7 @@ export const loginParams = async (req: any, res: any, next: any) => {
 
 /**
  * @desc 注册参数校验用户名、手机号、密码
- * @param {string} user_name - 用户名
+ * @param {string} userName - 用户名
  * @param {string} password - 密码
  * @param {string} phone - 手机号
  * @param {number} age - 年龄
@@ -78,8 +89,8 @@ export const loginParams = async (req: any, res: any, next: any) => {
  */
 export const registerParams = async (req: any, res: any, next: any) => {
   try {
-    const { user_name, password, phone, age, sex } = req.body
-    if (!user_name || !password || !phone) {
+    const { userName, password, phone, age, sex } = req.body
+    if (!userName || !password || !phone) {
       return res.status(400).json({
         code: 400,
         message: '用户名、密码和手机号都是必填项'
@@ -96,27 +107,33 @@ export const registerParams = async (req: any, res: any, next: any) => {
     }
 
     // 查询用户是否已存在
-    const checkUser: any = await mysql.query(loginService.checkUser, [user_name, phone] as never[])
+    const resultPhone: any = await mysql.query(loginService.checkUserAndPhone, [
+      userName,
+      phone
+    ] as never[])
 
-    if (checkUser.length > 0) {
+    if (resultPhone.length > 0) {
       return res.status(400).json({
         code: 400,
         message: '用户名或手机号已存在'
       })
     }
 
+    const { hash, salt } = hashPassword(password) // 密码加密
+
     const result: any = await mysql.query(loginService.addUser, [
-      user_name,
-      password,
+      userName,
       phone,
       age,
-      sex
+      sex,
+      hash, // password 加密后存储的哈希值
+      salt // 加密后存储的盐值
     ] as never[])
 
     // 验证通过，将用户信息添加到请求对象中
     req.userInfo = {
       userId: result.insertId,
-      userName: user_name,
+      userName,
       phone,
       age,
       sex
