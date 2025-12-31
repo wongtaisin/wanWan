233b7c2731d73e95facc9470dcf688a3d48aa14f
wingddd <wongtaisin1024@gmail.com>
2025-12-31 08:30:35 +0800

    user-jwt → userJwt；tsconfig：include，exclude 优化

--- src/util/user-jwt.ts
+++
@@ -1,9 +0,0 @@
-import { expressjwt } from 'express-jwt'
-
-// 验证token是否过期
-const jwtAuth = expressjwt({
-  secret: process.env.JWT_SECRET || 'wongtaisin', // 密匙
-  algorithms: ['HS256'] // 签名算法
-}).unless({ path: ['/api/login/signIn', '/api/login/signUp'] }) // unless 设置jwt认证白名单
-
-export default jwtAuth
